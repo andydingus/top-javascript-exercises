184b01f69bc1d0baa30f135710be2aad0a37dad5
Odmund Bautista <odfelip.bautista@gmail.com>
2023-10-26 15:38:44 -0500

    Complete tests 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // Solution using a for loop
+    let newString = '';
+    for (i = 0; i < num; i++) {
+        newString += string;
+    }
+    return newString;
+    // Easy solution
+    // return string.repeat(num);
 };
 
 // Do not edit below this line
