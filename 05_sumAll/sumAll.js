13119f8c4facdd7d80b3ac046e868250b43b01c2
Odmund Bautista <odfelip.bautista@gmail.com>
2023-10-29 16:36:28 -0500

    Complete Exercise 05
    Did it my own way and it was definitely a lot different than the given
    solution. I can understand my way better but I see why they went with
    the one they posted.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let finalSum;
+    // Checking for negative numbers...
+    if (firstNum < 0 || secondNum < 0) {
+        return 'ERROR';
+    // Making sure variables are numbers...
+    } else if (typeof firstNum !== 'number' || typeof secondNum !== 'number') {
+        return 'ERROR';
+    // If starting with smaller number...      
+    } else if (firstNum < secondNum) {
+        for (i = firstNum + 1; i < secondNum; i++) {
+            firstNum += i;
+        }
+        finalSum = firstNum + secondNum;
+    // If starting with bigger number...
+    } else {
+        for (i = firstNum - 1; i > secondNum; i--) {
+            firstNum += i;
+        }
+        finalSum = firstNum + secondNum;
+    }
+    
+    return finalSum;
 };
 
 // Do not edit below this line
