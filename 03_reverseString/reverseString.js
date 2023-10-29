fc82578d04c30db0210b0bea575f00b3dc35dd7c
Odmund Bautista <odfelip.bautista@gmail.com>
2023-10-29 13:41:11 -0500

    Finish Exercise 03 Made 2 solutions: one using the recommended way (splitting the string) and the other using a decrementing for-loop.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    // Splitting the string solution
+    let splitString = input.split("");
+    let reversedArray = splitString.reverse();
+    let newString = reversedArray.join("");
 
+    return newString;
+    
+    // For loop solution
+    // let newString = '';
+    // for (let i = input.length - 1; i >= 0; i--) {
+    //     newString += input[i];
+    // }
+
+    // return newString;
 };
 
 // Do not edit below this line
