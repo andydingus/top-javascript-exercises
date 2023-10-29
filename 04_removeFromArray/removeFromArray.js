a6a47a3ad4d2f20a66afd52e9ebad72fa48ddebf
Odmund Bautista <odfelip.bautista@gmail.com>
2023-10-29 14:04:40 -0500

    Complete Task 01

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, element) {
+    let elementIndex = arr.indexOf(element);
+    let newArray = arr;
+    if (elementIndex != -1) {
+        arr.splice(elementIndex, 1);
+    }
 
+    return newArray;
 };
 
 // Do not edit below this line
