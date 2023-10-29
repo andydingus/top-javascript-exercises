b5efcc43c907d4db943fb263479280f87f451384
Odmund Bautista <odfelip.bautista@gmail.com>
2023-10-29 17:14:09 -0500

    Complete Exercise 07
    Again, the given solution is much simpler compared to mine. I like add
    more lines to make the code more readable though.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahTemp) {
+  // Formula: C = (F - 32) x 5/9
+  let conversion = (fahTemp - 32) * (5 / 9);
+  let roundedConversion = Math.round(conversion * 10) / 10
+  return roundedConversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  // Formula: F = C x (9/5) + 32
+  let conversion = celTemp * (9/5) + 32
+  let roundedConversion = Math.round(conversion * 10) / 10
+  return roundedConversion;
 };
 
 // Do not edit below this line
