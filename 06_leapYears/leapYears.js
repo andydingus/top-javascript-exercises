4c8e418f45082ba0f2a559fc637743eebe96a38d
Odmund Bautista <odfelip.bautista@gmail.com>
2023-10-29 16:59:49 -0500

    Complete Exercise 06
    The given solution is much simpler than mine but I would argue mine is
    more readable.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// Leap years:
+// 1) Divisible by 4
+// 2) Not divisible by 100 unless divisible by 400
+    // Divisible by 4 and not divisible by 100 is a leap year
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    // Divisible by 100 but is also divisible by 400, making it a leap year
+    } else if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    // Not divisible by 4 and divisible by 100 is NOT a leap year
+    } else if (year % 4 !== 0 || year % 100 === 0){
+        return false;
+    }
+    
+ 
 };
 
 // Do not edit below this line
