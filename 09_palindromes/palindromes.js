51ef6d3d2be11a04a1eedb48f31ac507dc5548cf
Odmund Bautista <odfelip.bautista@gmail.com>
2024-01-25 10:16:40 -0600

    Finish Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const reversedString = str.toLowerCase().replace(/[^\w-]/g,"");
+    return reversedString.split("").reverse().join("") == reversedString;
 };
 
 // Do not edit below this line
