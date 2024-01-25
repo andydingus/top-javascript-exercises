4ee4ae6abb23e08ba4b855954bd6c4c483380ce6
Odmund Bautista <odfelip.bautista@gmail.com>
2024-01-25 10:31:45 -0600

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 2) {
+        if (n < 0) {
+            return 'OOPS';
+        }
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
